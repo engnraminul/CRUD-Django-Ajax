787a01ddbf8340d81446c44acd49aada40b05cbe
engnraminul <engnraminul@gmail.com>
2022-05-22 23:34:35 +0600

    Save data through ajax

--- AjaxApp/urls.py
+++ AjaxApp/urls.py
@@ -4,7 +4,7 @@ from .views import CrudHome, Home
 app_name = 'AjaxApp'
 
 urlpatterns = [
-    path('', CrudHome.as_view(), name= 'Home'),
+    #path('', CrudHome.as_view(), name= 'Home'),
     #path('create/', CrudCreate.as_view(), name='crud_create' ),
-    path('crud/', Home)
+    path('', Home)
 ]
